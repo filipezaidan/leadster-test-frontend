728716244690b47570c9ecb1d90b43074ba6a1eb
Filipe Zaidan <felipezaidan10@gmail.com>
2023-07-06 12:34:01 -0300

    styles:changed styles from  Button component

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -1,9 +1,10 @@
+import { ButtonHTMLAttributes } from "react";
 import { ButtonContainer } from "./styles";
 
-interface IButton {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export const Button = ({ children }: IButton) => {
-  return <ButtonContainer>{children}</ButtonContainer>;
+export const Button = ({ children, ...rest }: ButtonProps) => {
+  return <ButtonContainer {...rest}>{children}</ButtonContainer>;
 };
--- src/app/components/Button/styles.tsx
+++ src/app/components/Button/styles.tsx
@@ -1,25 +1,24 @@
 import { styled } from "styled-components";
 
 export const ButtonContainer = styled.button`
-  display: flex;
+  display: inline-block;
   align-items: center;
   justify-content: center;
-  height: 48px;
   border-radius: 45px;
   border: 1px solid #007dff;
   background-color: #0083ff;
   box-shadow: 0 20px 30px 0 rgba(12, 0, 46, 0.1);
   color: white;
-  padding: 14px 30px;
+  padding: 10px 20px;
   font-size: 1rem;
   font-weight: 600;
   cursor: pointer;
-  transition: 0.2s;
+  transition: all 0.3s linear;
+
   &:hover {
     color: #007dff;
     background: #fff;
     box-shadow: none;
     border: 1px solid #1857c1;
-    transition: all 0.3s linear;
   }
 `;

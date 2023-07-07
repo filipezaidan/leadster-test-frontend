91da59819ec76d84209f368b0910cadfd2ed940b
Filipe Zaidan <felipezaidan10@gmail.com>
2023-07-07 17:59:24 -0300

    feat: add media queries in HeroContent componet

--- src/app/components/HeroContent/index.tsx
+++ src/app/components/HeroContent/index.tsx
@@ -1,47 +1,35 @@
-"use client";
-
-import Image from "next/image";
 import {
-  Wrapper,
-  Container,
-  ContainerTitle,
-  Description,
-  ImageTitle,
-  Subtitle,
-  Tag,
-  TagTitle,
-  Title,
-  Divider,
-  Text,
+  HeroWrapper,
+  HeroContainer,
+  HeroContainerTitle,
+  HeroSubtitle,
+  HeroTag,
+  HeroTagTitle,
+  HeroTitle,
+  HeroDivider,
+  HeroDescription,
+  HeroTitleBackground,
 } from "./styles";
 
-// TODO: adjust gap between the elements
 export const HeroContent = () => {
   return (
-    <Wrapper>
-      <Container>
-        <Tag>
-          <TagTitle>WEBINARS EXCLUSIVOS</TagTitle>
-        </Tag>
-        <Subtitle>Menos Conversinha,</Subtitle>
-        <ContainerTitle>
-          <Title>Mais Conversação</Title>
-          <Description>{`#2c83fb -> #1f76f0 (-45º)`}</Description>
-          <ImageTitle>
-            <Image
-              width={49}
-              height={32}
-              src="/asset-header.png"
-              alt={`Icone acima da letra "o" do texto "Mais Conversação" `}
-            />
-          </ImageTitle>
-        </ContainerTitle>
-        <Divider />
-        <Text>
+    <HeroWrapper>
+      <HeroContainer>
+        <HeroTag>
+          <HeroTagTitle>WEBINARS EXCLUSIVOS</HeroTagTitle>
+        </HeroTag>
+        <HeroSubtitle>Menos Conversinha,</HeroSubtitle>
+        <HeroContainerTitle>
+          <HeroTitle>
+            <HeroTitleBackground>Mais Conversação</HeroTitleBackground>
+          </HeroTitle>
+        </HeroContainerTitle>
+        <HeroDivider />
+        <HeroDescription>
           Conheça as estratégias que <strong>mudaram o jogo</strong> e como
           aplicá-las no seu negócio
-        </Text>
-      </Container>
-    </Wrapper>
+        </HeroDescription>
+      </HeroContainer>
+    </HeroWrapper>
   );
 };
--- src/app/components/HeroContent/styles.tsx
+++ src/app/components/HeroContent/styles.tsx
@@ -1,83 +1,161 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.section`
+export const HeroWrapper = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 600px;
+  height: 500px;
   background-color: #f0f8ff;
+
+  @media (min-width: 576px) {
+    height: 600px;
+  }
 `;
 
-export const Container = styled.section`
+export const HeroContainer = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 50%;
+  width: 90%;
+
+  @media (min-width: 410px) {
+    width: 82%;
+  }
+  @media (min-width: 576px) {
+    width: 80%;
+  }
+
+  @media (min-width: 768px) {
+    width: 65%;
+  }
+
+  @media (min-width: 992px) {
+    width: 63%;
+  }
+
+  @media (min-width: 1200px) {
+    width: 62%;
+  }
+
+  @media (min-width: 1400px) {
+    width: 55%;
+  }
 `;
 
-export const Tag = styled.div`
+export const HeroTag = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   border: 2px solid #0077f4;
   background-color: transparent;
-  padding: 8px 16px;
-  min-width: 208px;
+  padding: 4px 8px;
+  white-space: nowrap;
+  min-width: 20%;
+
   width: auto;
   border-radius: 24px 24px 24px 4px;
 `;
 
-export const TagTitle = styled.span`
-  font-size: 1rem;
+export const HeroTagTitle = styled.span`
+  font-size: 0.65rem;
   font-weight: 600;
   color: #0077f4;
+
+  @media (min-width: 576px) {
+    font-size: 0.75rem;
+  }
+
+  @media (min-width: 992px) {
+    font-size: 1.2rem;
+  }
 `;
 
-export const ContainerTitle = styled.div`
+export const HeroContainerTitle = styled.div`
   display: flex;
   flex-direction: column;
   position: relative;
+  white-space: nowrap;
 `;
 
-export const Title = styled.span`
-  font-size: 5.2rem;
+export const HeroTitle = styled.span`
+  font-size: 2.3rem;
   font-weight: 700;
+  height: auto;
   font-style: normal;
   background-clip: text;
   -webkit-background-clip: text;
   color: transparent;
-  background-image: linear-gradient(to right, #0084ff, #0077f4);
+  background-image: linear-gradient(-45deg, #2c83fb, #1f76f0);
+
+  @media (min-width: 576px) {
+    font-size: 3rem;
+  }
+
+  @media (min-width: 768px) {
+    font-size: 3rem;
+  }
+
+  @media (min-width: 992px) {
+    font-size: 4.8rem;
+  }
+
+  @media (min-width: 1200px) {
+    font-size: 5.2rem;
+  }
 `;
 
-export const Subtitle = styled.span`
+export const HeroTitleBackground = styled.span`
+  background-image: url("/asset-header.png");
+  background-position: right -1% bottom 82%;
+  background-repeat: no-repeat;
+  background-size: 6%;
+`;
+
+export const HeroSubtitle = styled.span`
   color: #1c3c50;
-  font-size: 3.8rem;
+  font-size: 1.6rem;
   font-weight: 500;
+  white-space: nowrap;
+
+  @media (min-width: 576px) {
+    font-size: 2rem;
+  }
+  @media (min-width: 768px) {
+    font-size: 2rem;
+  }
+
+  @media (min-width: 992px) {
+    font-size: 3.2rem;
+  }
+
+  @media (min-width: 1200px) {
+    font-size: 3.4rem;
+  }
 `;
 
-export const ImageTitle = styled.div`
-  position: absolute;
-  top: 15px;
-  right: -18px;
-  width: 49px;
-  height: 32px;
-`;
-
-export const Description = styled.span`
-  color: #ff4a58;
-`;
-
-export const Divider = styled.div`
+export const HeroDivider = styled.div`
   width: 100%;
   height: 2px;
   background-color: #c8d4dd;
   margin: 10px 0;
+  white-space: nowrap;
 `;
 
-export const Text = styled.p`
-  font-size: 1.2rem;
+export const HeroDescription = styled.p`
+  font-size: 0.8rem;
   color: #1c3c50;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    font-size: 1rem;
+  }
+  @media (min-width: 992px) {
+    font-size: 1.1rem;
+  }
+  @media (min-width: 1200px) {
+    font-size: 1.25rem;
+  }
 `;

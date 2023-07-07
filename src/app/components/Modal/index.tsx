a43f28ed1719d798838499e52c39174984328d75
Filipe Zaidan <felipezaidan10@gmail.com>
2023-07-07 13:05:14 -0300

    feat: add Modal component

--- src/app/components/Modal/index.tsx
+++ src/app/components/Modal/index.tsx
@@ -1,19 +1,68 @@
 import {
+  CloseButton,
+  CloseContainer,
+  DescriptionContainer,
+  DescriptionText,
+  DescriptionTitle,
+  Divider,
+  DownaloadsButtonsContainer,
+  DownloadsContainer,
   LineHeader,
   ModalBody,
   ModalContainer,
   ModalFooter,
   ModalHeader,
+  ModalHeaderText,
+  ModalHeaderTitle,
   ModalOverlay,
+  VideoYoutube,
 } from "./styles";
+import { DownloadButton } from "../DownloadButton";
+
 export const Modal = () => {
   return (
     <ModalOverlay>
       <ModalContainer>
         <LineHeader />
-        <ModalHeader></ModalHeader>
-        <ModalBody></ModalBody>
-        <ModalFooter></ModalFooter>
+        <CloseContainer onClick={() => alert("fechar modal")}>
+          <CloseButton size={20} color="#718493" />
+        </CloseContainer>
+        <ModalHeader>
+          <ModalHeaderTitle>
+            Webinar:{" "}
+            <ModalHeaderText>
+              Como aumentar sua Geração de Leads feat. Traktor
+            </ModalHeaderText>
+          </ModalHeaderTitle>
+        </ModalHeader>
+
+        <ModalBody>
+          <VideoYoutube
+            src="https://www.youtube.com/embed/ouCGdcnxeu8"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          />
+        </ModalBody>
+        <ModalFooter>
+          <DescriptionContainer>
+            <DescriptionTitle>Descrição</DescriptionTitle>
+            <Divider />
+            <DescriptionText>
+              Lorem ipsum dolor sit amet, consectetur adipng elit.Morbi cursus
+              massa. Lorem ipsum dolor sit amet, consectetur adipng elit.Morbi
+              cursus massa. Lorem ipsum dolor sit amet
+            </DescriptionText>
+          </DescriptionContainer>
+          <DownloadsContainer>
+            <DescriptionTitle>Downloads</DescriptionTitle>
+            <Divider />
+            <DownaloadsButtonsContainer>
+              <DownloadButton type="xls" />
+              <DownloadButton type="doc" />
+              <DownloadButton type="ppt" />
+            </DownaloadsButtonsContainer>
+          </DownloadsContainer>
+        </ModalFooter>
       </ModalContainer>
     </ModalOverlay>
   );
--- src/app/components/Modal/styles.tsx
+++ src/app/components/Modal/styles.tsx
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import { MdClose as CloseIcon } from "react-icons/md";
 
 export const ModalOverlay = styled.div`
   position: fixed;
@@ -7,7 +8,7 @@ export const ModalOverlay = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5); /* Transparência do overlay */
+  background-color: rgba(29, 61, 81, 0.6);
   display: flex;
   align-items: center;
   justify-content: center;
@@ -18,8 +19,10 @@ export const ModalContainer = styled.div`
   flex-direction: column;
   border-radius: 16px;
   position: relative;
-  width: 30%;
-  height: 70%;
+  min-height: 480px;
+  min-width: 300px;
+  width: 32%;
+  height: 72%;
   background-color: white;
   overflow: hidden;
 `;
@@ -27,14 +30,34 @@ export const ModalContainer = styled.div`
 export const ModalHeader = styled.div`
   display: flex;
   width: 100%;
-  height: 35%;
+  justify-content: center;
+  padding: 0% 0% 5% 15%;
+`;
+
+export const CloseContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+  padding: 4px 8px;
+`;
+
+export const CloseButton = styled(CloseIcon)`
+  cursor: pointer;
+`;
+
+export const ModalHeaderTitle = styled.span`
+  font-size: 1.4rem;
+  font-weight: 600;
+  color: #007eff;
+`;
+export const ModalHeaderText = styled.span`
+  color: #1c3c50;
 `;
 
 export const LineHeader = styled.div`
   display: flex;
   width: 100%;
-  height: 6px;
-
+  height: 4px;
   background-color: #007eff;
 `;
 
@@ -42,6 +65,59 @@ export const ModalTitle = styled.span`
   font-size: 1.2rem;
 `;
 
-export const ModalBody = styled.div``;
+export const ModalBody = styled.div`
+  display: flex;
+  width: 100%;
+  flex: 1;
+`;
 
-export const ModalFooter = styled.div``;
+export const VideoYoutube = styled.iframe`
+  display: flex;
+  border: none;
+  width: 100%;
+`;
+
+export const ModalFooter = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  gap: 1rem;
+  padding: 4%;
+`;
+
+export const DescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 10px;
+`;
+
+export const DescriptionTitle = styled.span`
+  font-size: 1rem;
+  color: #1c3c50;
+  font-weight: 600;
+`;
+
+export const DescriptionText = styled.p`
+  font-size: 1rem;
+  color: #1c3c50;
+`;
+
+export const Divider = styled.div`
+  display: flex;
+  width: 100%;
+  height: 1px;
+  border: 1px solid #eaecee;
+`;
+
+export const DownloadsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 10px;
+`;
+
+export const DownaloadsButtonsContainer = styled.div`
+  display: flex;
+  gap: 10px;
+`;
